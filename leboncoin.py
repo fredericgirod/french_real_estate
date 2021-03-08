aa64b57869aaed4ef7aed11c295a81bb039610f2
Frederic Girod <fred.fredericgirod@gmail.com>
2021-03-08 20:43:14 +0100

    Update leboncoin.py

--- leboncoin.py
+++ leboncoin.py
@@ -20,13 +20,13 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 import plotly.graph_objects as go
 import altair as alt
 import streamlit as st
-# import openpyxl
 
 # os.chdir('E:\Workarea\Python\Webcrawling')
 
 date = "21/02/2021"
 
-df_data = pd.read_excel('Terrain Cessy.xlsx',sheet_name='PdG').iloc[:46,:]
+# df_data = pd.read_excel('Terrain Cessy.xlsx', sheet_name='PdG').iloc[:46,:]
+df_data = pd.read_excel('Terrain Cessy.xlsx', sheet_name='PdG', engine='openpyxl')
 # wb = load_workbook('Terrain Cessy.xlsx')
 # ws = wb['PdG']
 # df_data = pd.DataFrame(ws.values).iloc[:46,:]
