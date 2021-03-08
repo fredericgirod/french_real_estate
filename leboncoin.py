20a03cdc0e495cbcd8ba82bceb9dde20e0f00e0d
Frederic Girod <fred.fredericgirod@gmail.com>
2021-03-08 21:17:36 +0100

    Update leboncoin.py

--- leboncoin.py
+++ leboncoin.py
@@ -169,7 +169,7 @@ st.write('')
 # Get longitude and latitude of countries
 lon = df_data['longitude']
 lat = df_data['latitude']
-df_map_data = pd.DataFrame({'lat': lat, 'lon': lon}).to_numeric()
+df_map_data = pd.to_numeric(pd.DataFrame({'lat': lat, 'lon': lon}))
 st.map(df_map_data, zoom=11, use_container_width=True, )
 st.write('')
 
